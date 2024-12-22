45be912abeef2d9af2b708b619a5abca30858e8e
ArtyomArtenyan <artyomartenyan.ait@gmail.com>
2024-12-22 10:31:54 +0400

    created initial files for Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Header } from './index.js';
 const App = () => {
 	return (
-		<div>
-			{' '}
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
-		</div>
+		<>
+			<Header />
+		</>
 	);
 };
 
